24987b7a5d65ae09eab75d40aa1d2eadd91de320
hunit <hunit612@naver.com>
2024-09-19 00:41:06 +0900

    feat: add first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,11 +31,11 @@ dependencies {
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
     testImplementation("io.kotest:kotest-assertions-core:5.6.2")
-    testImplementation("io.kotest:extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
