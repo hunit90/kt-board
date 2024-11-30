63118ebcae42f9f255f57147e7ff9298a92f61ce
이기훈 <huni@igihun-ui-MacBookPro.local>
2024-12-01 00:45:05 +0900

    first commit

---
+++ src/test/kotlin/com/kotlin/ktboard/KtBoardApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.kotlin.ktboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
---
+++ src/test/kotlin/com/kotlin/ktboard/SampleTest.kt
@@ -0,0 +1,10 @@
+package com.kotlin.ktboard
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class SampleTest : FunSpec({
+    test("sample test") {
+        1 shouldBe 1
+    }
+})
